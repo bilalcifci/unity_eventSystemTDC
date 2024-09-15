ebae25ad21846b6b528c8145b87933c1678743d8
Bilal CIFCI <cifci.bilal@outlook.com>
2024-09-15 18:52:06 +0300

    EventSystem | EventTrigger | Drag, Click vb.

---
+++ Assets/SuruklemeIsi.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SuruklemeIsi : MonoBehaviour,IDragHandler,IEndDragHandler,IPointerDownHandler,IPointerUpHandler
+{
+
+    public GameObject Panel;
+    Vector3 ilkpoziyon;
+    void Start()
+    {
+        ilkpoziyon = transform.position;
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+      
+        transform.position = Input.mousePosition;       
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+       
+        float mesafe = Vector3.Distance(transform.position , Panel.transform.position);
+
+        if (mesafe<160)
+        {
+            transform.SetParent(Panel.transform);
+
+        }
+        else
+        {
+
+            transform.position = ilkpoziyon;
+        }
+       // Debug.Log(mesafe.ToString());
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("basıldı");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("bırakıldı");
+    }
+}
